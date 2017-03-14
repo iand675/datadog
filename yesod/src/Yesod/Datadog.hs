2979f9024f2e3d857d27d342607ee033b95a6bd3
Ian Duncan <ian@iankduncan.com>
2017-03-14 13:48:40 +0900

    Only want route name, not route params

--- yesod/src/Yesod/Datadog.hs
+++ yesod/src/Yesod/Datadog.hs
@@ -1,25 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-module Lib
+module Yesod.Datadog
     ( ddMiddleware
     ) where
 
 import Control.Monad
 import Data.IORef
 import Data.Text (Text, pack)
+import Data.Typeable
 import Data.Vault.Lazy as L
 import Network.StatsD.Datadog
 import Network.Wai
 import Network.Wai.Middleware.Datadog
 import Yesod.Core
 
-ddMiddleware :: (Show (Route site), ToTypedContent res) => HandlerT site IO res -> HandlerT site IO res
+ddMiddleware :: (Typeable (Route site), ToTypedContent res) => HandlerT site IO res -> HandlerT site IO res
 ddMiddleware m = do
   mRoute <- getCurrentRoute
   req <- waiRequest
   liftIO $ case L.lookup statsTagsKey $ vault req of
     Just ref -> case mRoute of
       Nothing -> modifyIORef ref (tag "route_name" "none" :)
-      Just r -> modifyIORef ref (tag "route_name" (pack $ show r) :)
+      Just r -> modifyIORef ref (tag "route_name" (pack $ tyConName $ typeRepTyCon $ typeOf r) :)
+    Nothing -> return ()
   m
