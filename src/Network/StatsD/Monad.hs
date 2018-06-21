c90a277439ec603705862acbf6848cc0a92ad702
John Ky <newhoggy@gmail.com>
2018-06-21 15:36:36 +1000

    Add mtl typeclass instances

--- src/Network/StatsD/Monad.hs
+++ src/Network/StatsD/Monad.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
-module Network.StatsD.Monad
-where
 
-import Control.Monad.IO.Class
+module Network.StatsD.Monad where
+
+import Control.Monad.Except
+import Control.Monad.Reader
 import Network.StatsD.Datadog
 
 class (Functor m, Applicative m, MonadIO m) => MonadStats m where
   getStatsClient :: m StatsClient
+
+instance MonadStats m => MonadStats (ExceptT e m) where
+  getStatsClient = lift getStatsClient
+
+instance MonadStats m => MonadStats (ReaderT e m) where
+  getStatsClient = lift getStatsClient
 
 sendMetric :: MonadStats m => Metric -> m ()
 sendMetric m = getStatsClient >>= flip sendSampled m
